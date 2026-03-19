6d2b72869a5634c50f6fd43b5f89bc7cc7389b60
Amit <amitmahich40@gmail.com>
2026-03-19 19:12:38 +0530

    final

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,36 +1,3 @@
-// const nodemailer = require("nodemailer");
-
-// const sendEmail = async ({ to, subject, html }) => {
-//   // ⚡ Create the transporter HERE, inside the function
-//   const transporter = nodemailer.createTransport({
-//     host: "smtp.gmail.com",
-//     port: 587,
-//     secure: false,
-//     family: 4,
-//     auth: {
-//       user: process.env.EMAIL_USER,
-//       pass: process.env.EMAIL_PASS,
-//     },
-//     tls: {
-//       rejectUnauthorized: false,
-//     },
-//   });
-
-//   try {
-//     const info = await transporter.sendMail({
-//       from: `"SecureFind" <${process.env.EMAIL_USER}>`,
-//       to,
-//       subject,
-//       html,
-//     });
-//     console.log("✅ Email sent:", info.response);
-//   } catch (error) {
-//     console.error("❌ Email sending error:", error);
-//     throw error;
-//   }
-// };
-
-// module.exports = sendEmail;
 const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 const sendEmail = async ({ to, subject, html }) => {
