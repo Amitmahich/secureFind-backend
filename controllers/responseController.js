4591919344059b17abf16bc05c25e08724e58409
Amit <amitmahich40@gmail.com>
2026-03-24 22:30:40 +0530

    updated

--- controllers/responseController.js
+++ controllers/responseController.js
@@ -40,7 +40,6 @@ const getMyResponsesController = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
-
 // submit answer
 const submitAnswerController = async (req, res) => {
   try {
@@ -166,10 +165,27 @@ const updateResponseStatusController = async (req, res) => {
       console.log(err);
       console.log("Email failed but approval done");
     }
-    ////////////////////////////////SEND EMAIL END
+    ////////////////////////////////SEND EMAIL END/////////////////////////////////////
 
+    const itemId = response.item._id;
+
+    const item = await itemModel.findById(itemId);
+    if (!item) {
+      return res.status(404).send({
+        success: false,
+        message: "Item not found",
+      });
+    }
+
+    // ADD HERE
+    if (item.status === "CLAIMED") {
+      return res.status(400).json({
+        success: false,
+        message: "Item already claimed",
+      });
+    }
     // optionally mark item as approved
-    response.item.status = "APPROVED";
+    response.item.status = "CLAIMED";
     await response.item.save();
     //////////////////////////////////SOCKET PART START//////////////////////////////////
     const io = getIO();
@@ -200,6 +216,7 @@ const updateResponseStatusController = async (req, res) => {
       message: "Response approved successfully",
     });
   } catch (error) {
+    console.log(error);
     res.status(500).json({
       message: error.message,
     });
--- models/itemModel.js
+++ models/itemModel.js
@@ -57,7 +57,7 @@ const itemSchema = new mongoose.Schema(
     },
   },
   {
-    timestamps: true, // createdAt + updatedAt
+    timestamps: true,
   },
 );
 
